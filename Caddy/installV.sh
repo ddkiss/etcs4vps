0c6b815f347989b7a29d222cd2f8cb785fc33bc1
ddkiss <film@whu.edu.cn>
2018-03-24 09:32:27 +0800

    Delete installV.sh

--- Caddy/installV.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir /etc/v2ray
-mkdir /var/log/v2ray
-nano /etc/v2ray/config.json
-bash <(curl -L -s https://install.direct/go.sh)
-systemctl enable v2ray
-systemctl start v2ray
-systemctl status v2ray
