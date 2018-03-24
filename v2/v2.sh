df67491a1fbb6c636266322ac634929e6e33b677
ddkiss <film@whu.edu.cn>
2018-03-24 18:30:39 +0800

    Add files via upload

---
+++ v2/v2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir /etc/v2ray
+mkdir /var/log/v2ray
+nano /etc/v2ray/config.json
+bash <(curl -L -s https://install.direct/go.sh)
+systemctl enable v2ray
+systemctl start v2ray
+systemctl status v2ray
