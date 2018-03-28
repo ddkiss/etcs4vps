beebe42db7865df2cbd5ac759e1a8320c7ce0f01
ddkiss <film@whu.edu.cn>
2018-03-28 22:52:32 +0800

    Update installCaddy.sh

--- Caddy/installCaddy.sh
+++ Caddy/installCaddy.sh
@@ -5,6 +5,7 @@ mkdir /var/www
 curl https://getcaddy.com | bash -s personal
 curl -s https://raw.githubusercontent.com/ddkiss/etcs4vps/master/Caddy/caddy.service -o /etc/systemd/system/caddy.service
 nano /etc/caddy/Caddyfile
+ulimit -n 8192
 systemctl daemon-reload
 systemctl enable caddy.service
 systemctl start caddy.service
