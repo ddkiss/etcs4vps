7114445f0c1b8adeb63799a0b69e2ce4d72ef81b
ddkiss <film@whu.edu.cn>
2018-04-11 14:47:48 +0800

    Update install.sh

--- v2/install.sh
+++ v2/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 mkdir /etc/v2ray
 mkdir /var/log/v2ray
-nano /etc/v2ray/config.json
 bash <(curl -L -s https://install.direct/go.sh)
-systemctl enable v2ray
+nano /etc/v2ray/config.json
 systemctl start v2ray
 systemctl status v2ray
