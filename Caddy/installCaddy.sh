eb6eb9bb94c2231d9e444aaf4abb3fc1d4b46dd9
ddkiss <film@whu.edu.cn>
2018-05-18 15:31:29 +0800

    Update installCaddy.sh

--- Caddy/installCaddy.sh
+++ Caddy/installCaddy.sh
@@ -8,8 +8,8 @@ chown -R www-data:root /etc/ssl/caddy
 mkdir /var/www
 chown www-data:www-data /var/www
 curl https://getcaddy.com | bash -s personal http.forwardproxy
-curl -s https://raw.githubusercontent.com/ddkiss/etcs4vps/master/Caddy/caddy.service -o /etc/systemd/system/caddy.service
 nano /etc/caddy/Caddyfile
+curl -s https://raw.githubusercontent.com/ddkiss/etcs4vps/master/Caddy/caddy.service -o /etc/systemd/system/caddy.service
 systemctl daemon-reload
 systemctl enable caddy.service
 systemctl start caddy.service
