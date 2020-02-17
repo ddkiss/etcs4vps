8c47f0920d41854f71da0fc8241c341f9565ebb5
ddkiss <film@whu.edu.cn>
2020-02-17 17:02:30 +0800

    Create install.sh

---
+++ trae/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir /etc/traefik
+mkdir /etc/traefik/dy
+mkdir /usr/bin/traefik
+wget https://github.com/containous/traefik/releases/download/v2.1.4/traefik_v2.1.4_linux_amd64.tar.gz
+tar -zxvf traefik_v2.1.4_linux_amd64.tar.gz -C /usr/bin/traefik
+nano /etc/traefik/traefik.toml
+nano /etc/traefik/dy/1.toml
+curl -s https://raw.githubusercontent.com/ddkiss/etcs4vps/master/Caddy/caddy.service -o /etc/systemd/system/caddy.service
+systemctl daemon-reload
+systemctl enable traefik
+systemctl start traefik
+systemctl status traefik
