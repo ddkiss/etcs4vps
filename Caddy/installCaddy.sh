974041fc0c09b68d149990f96ffcb89cb05abfe2
ddkiss <film@whu.edu.cn>
2020-02-17 17:29:48 +0800

    Update installCaddy.sh

--- Caddy/installCaddy.sh
+++ Caddy/installCaddy.sh
@@ -7,7 +7,7 @@ chown -R root:www-data /etc/ssl/caddy
 chmod 0770 /etc/ssl/caddy
 mkdir /var/www
 chown www-data:www-data /var/www
-curl https://getcaddy.com
+curl https://getcaddy.com | bash -s personal
 nano /etc/caddy/Caddyfile
 curl -s https://raw.githubusercontent.com/ddkiss/etcs4vps/master/Caddy/caddy.service -o /etc/systemd/system/caddy.service
 systemctl daemon-reload
