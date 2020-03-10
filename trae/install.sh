28137931563cde77a0da8bb520e874bc5e44750f
ddkiss <film@whu.edu.cn>
2020-03-10 18:42:21 +0800

    Update install.sh

--- trae/install.sh
+++ trae/install.sh
@@ -2,8 +2,8 @@
 mkdir /etc/traefik
 mkdir /etc/traefik/dy
 mkdir /usr/bin/traefik
-wget https://github.com/containous/traefik/releases/download/v2.1.4/traefik_v2.1.4_linux_amd64.tar.gz
-tar -zxvf traefik_v2.1.4_linux_amd64.tar.gz -C /usr/bin/traefik
+wget https://github.com/containous/traefik/releases/download/v2.1.6/traefik_v2.1.6_linux_amd64.tar.gz
+tar -zxvf traefik_v2.1.6_linux_amd64.tar.gz -C /usr/bin/traefik
 nano /etc/traefik/traefik.toml
 nano /etc/traefik/dy/1.toml
 curl -s https://raw.githubusercontent.com/ddkiss/etcs4vps/master/trae/traefik.service -o /etc/systemd/system/traefik.service
