09d7bd43e60c0cc0a178898dd3e2a1b3cf60027e
ddkiss <film@whu.edu.cn>
2020-04-23 16:10:04 +0800

    Update 2.sh

--- gost/2.sh
+++ gost/2.sh
@@ -1,39 +1,37 @@
 #!/bin/sh
-#read -p "host name?" HOSTNAME
-read -p "pwd?" PPWD
-read -p "gost version(ex 2.11.0)?" VERNO
-read -p "ssport?" S1PORT
-read -p "s5port?" S2PORT
-
-
-echo "download gost"
-wget https://github.com/ginuerzh/gost/releases/download/v${VERNO}/gost-linux-amd64-${VERNO}.gz
-gzip -d  gost-linux-amd64-${VERNO}.gz
-mkdir /usr/bin/gost
-mv gost-linux-amd64-${VERNO} /usr/bin/gost/gost
-chmod +x /usr/bin/gost/gost
-
-#echo "ln sslcert"
-#ln -s /var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/${HOSTNAME}/${HOSTNAME}.crt /usr/bin/gost/cert.pem
-#ln -s /var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/${HOSTNAME}/${HOSTNAME}.key /usr/bin/gost/key.pem
-
-echo "Generate /etc/gost/config.json"
-cat <<EOF > /etc/gost/config.json
+read -p "-L method?(tcp://:993" METHOD1
+read -p "-F method?(relay://:990)" METHOD2
+echo "install gost"
+snap install gost
+echo "Generate /snap/gost/config.json"
+cat <<EOF > /snap/gost/config.json
 {
     "ServeNodes": [
-        "ss://AEAD_CHACHA20_POLY1305:${PPWD}@:${S1PORT}",
-        "ssu://AEAD_CHACHA20_POLY1305:${PPWD}@:${S1PORT}",
-        "socks5+tls://dd:${PPWD}@:${S2PORT}"
-    ]
+        "${METHOD1}"
+    ],
+	"ChainNodes": [
+        "${METHOD2}"
+     ]
+#    ],
+#	"Routes": [
+#        {
+#            "Retries": 1,
+#            "ServeNodes": [
+#                "ws://:1443"
+#            ],
+#            "ChainNodes": [
+#                "socks://:192.168.1.1:1080"
+#            ]
+#        }        
+#    ]
 }
 EOF
-
 echo "Generate /etc/systemd/system/gost.service"
 cat <<EOF > /etc/systemd/system/gost.service
 [Unit]
 Description=gost
 [Service]
-ExecStart=/usr/bin/gost/gost -C /etc/gost/config.json
+ExecStart=/snap/bin/gost -C /snap/gost/config.json
 Restart=always
 User=root
 [Install]
