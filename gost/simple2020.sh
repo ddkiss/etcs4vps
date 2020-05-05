383a284b2b425ad68649bf714238e7211eae0e21
ddkiss <film@whu.edu.cn>
2020-05-05 17:06:39 +0800

    Update simple2020.sh

--- gost/simple2020.sh
+++ gost/simple2020.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 read -p "method?(-L udp://:1053 -L tcp://:1053 -F relay://:12345)" METHOD1
-echo "install gost"
-snap install gost
+#echo "install gost"
+#snap install gost
 
 echo "Generate /etc/systemd/system/gost.service"
 cat <<EOF > /etc/systemd/system/gost.service
