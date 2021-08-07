1d04a7285550d46d8e8677247eaca77720274824
ddkiss <film@whu.edu.cn>
2021-08-07 11:21:53 +0800

    Update install.sh

--- v2/install.sh
+++ v2/install.sh
@@ -5,8 +5,9 @@ cat <<EOF > /etc/v2ray/datupdate.sh
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
 EOF
 chmod 777 /etc/v2ray/datupdate.sh
-nano /etc/v2ray/config.json
+
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+nano /usr/local/etc/v2ray/config.json
+chrontab -e
 systemctl restart v2ray
 systemctl status v2ray
-chrontab -e
